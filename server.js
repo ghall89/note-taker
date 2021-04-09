75dfeb491c911875f4298444a2d020d1c88e033e
Graham Hall <ghall89@me.com>
2021-04-09 18:20:27 +0000

    linked up the front end

--- server.js
+++ server.js
@@ -7,12 +7,24 @@ const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
 
 app.get('/api/notes', (req, res) => {
 	res.json(notes);
+});
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 app.post('/api/notes', (req, res) => {	
